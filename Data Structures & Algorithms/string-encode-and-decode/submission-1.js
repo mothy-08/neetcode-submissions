6dba4733097f4986844b10b5779d7a05c3a85282
Timothy Dave Alimagno <119960367+mothy-08@users.noreply.github.com>
2026-06-24 14:16:40 +0800

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encodedStr = "";
+
+        for (const str of strs) { 
+            encodedStr += String(str.length) + "#" + str;
+        }
+
+        console.log(encodedStr);
+
+        return encodedStr;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const decodedStrs = [];
+        let i = 0;
+
+        while (i < str.length) {
+            let j = i
+
+            while (str[j] != "#")  j += 1
+            
+            const n = Number(str.slice(i, j));
+            i = j + 1;
+            j = i + n;
+
+            decodedStrs.push(str.slice(i, j));
+            i = j;
+        }
+
+        return decodedStrs;
+    }
+}
