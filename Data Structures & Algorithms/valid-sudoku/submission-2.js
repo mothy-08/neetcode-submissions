08eb0a1637f3628e704c19722ccf2fb9ea9d2605
Timothy Dave Alimagno <119960367+mothy-08@users.noreply.github.com>
2026-06-26 11:17:58 +0800

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        const rows = {};
+        const cols = {};
+        const sqrs = {};
+
+        for (let i = 0; i < 9; ++i) {
+            for (let j = 0; j < 9; ++j) {
+                if (board[i][j] == ".") continue;
+
+                const sqr =  ~~(i / 3) * 3 + ~~(j / 3); 
+
+                if (!rows[i]) rows[i] = new Set();
+                if (!cols[j]) cols[j] = new Set();
+                if (!sqrs[sqr]) sqrs[sqr] = new Set();
+
+                if (
+                    rows[i].has(board[i][j]) ||
+                    cols[j].has(board[i][j]) ||
+                    sqrs[sqr].has(board[i][j])
+                )
+                    return false;
+
+                rows[i].add(board[i][j]);
+                cols[j].add(board[i][j]);
+                sqrs[sqr].add(board[i][j]);
+            }
+        }
+
+        return true;
+    }
+}
