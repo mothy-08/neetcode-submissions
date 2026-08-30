081187a893f8fa958ab5ceaf2c0eb2bfd99db096
Timothy Dave Alimagno <119960367+mothy-08@users.noreply.github.com>
2026-08-31 06:40:09 +0800

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        var chars = Array(repeating: 0, count: 26)
+        let aAscii: UInt8 = 97
+
+        for c in s {
+            if let charAcii = c.asciiValue {
+                chars[Int(charAcii - aAscii)] += 1
+            }
+        }
+
+        for c in t {
+            if let charAcii = c.asciiValue {
+                chars[Int(charAcii - aAscii)] -= 1
+            }
+        }
+
+        return chars.allSatisfy({$0 == 0})
+    }
+}
