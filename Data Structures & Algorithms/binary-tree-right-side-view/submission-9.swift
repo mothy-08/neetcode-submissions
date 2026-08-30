cc3a48966c6bb5e7c4a12340fb304dfc42d98327
Timothy Dave Alimagno <119960367+mothy-08@users.noreply.github.com>
2026-08-30 09:50:45 +0800

    Add: binary-tree-right-side-view - submission-9

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-9.swift
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func rightSideView(_ root: TreeNode?) -> [Int] {
+        var res: [Int] = []
+
+        func dfs(_ node: TreeNode?, _ depth: Int) {
+            guard let node else { return }
+
+            if res.count == depth {
+                res.append(node.val)
+            } 
+
+            dfs(node.right, depth + 1)
+            dfs(node.left, depth + 1)
+        }
+
+        dfs(root, 0)
+        return res
+    }
+}
